b09679c15556b2e15aeaf5a0813b85ec31418ad3
issam-nz <is.elouafa@aulanz.net>
2022-10-16 15:39:12 +0200

    anadir comentario en la clase

--- Contrasena.java
+++ Contrasena.java
@@ -2,6 +2,9 @@ import java.util.Random;
 
 public class Contrasena {
     private String textoContrasena;
+    
+    //lista de los caracteres permitidos
+    // en la tabla ascii desde 32-122 sin dos caracteres(39, 44)->(',)
     private char[] listaChar = {32, 33, 34, 35, 36, 37, 38, 40, 41, 42,
             43, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56,
             57, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71,
