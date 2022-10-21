256629d5fd9d13dfef5590cc694135389bd69cda
issam <is.elouafa@aulanz.net>
2022-10-21 13:51:09 +0200

    update aleratorio num

--- AlertatorioNum.java
+++ AlertatorioNum.java
@@ -2,102 +2,92 @@ import java.util.Scanner;
 
 public class NumSecreto {
 
-    private int NumAler;
-    private int intentos;
+    private int numAler;
+    private int intentos = 0;
     private final int intentosMax = 3;
 
 
     public NumSecreto() {
-        NumAler = (int) (Math.random()*100) +1;
+        numAler = (int) (Math.random()*100) +1;
     }
-//
-//    public boolean esExito(int x) {
-//        return NumAler == x;
-//    }
-//
-//    public String pista(int x) {
-//
-//        if (x > NumAler) {
-//            return "mayor";
-//        } else {
-//            return "menor";
-//        }
-//    }
-//
-//    public String resultado(int x) {
-//        String mensaje = "";
-//        while (intentos <= intentosMax) {
-//            if (esExito(x)) {
-//                mensaje = "exito";
-//                break;
-//            } else {
-//                intentos += 1;
-//                return pista(x);
-//            }
-//
-//        }
-//
-//    }
-//if (NumAler < 10) {} else if (NumAler > 90) {} else {// 10 < num < 90}
+    public void mostrarNum() {
+        System.out.println(numAler);
+    }
+
+    public boolean esExito(int x) {
+        return numAler == x;
+    }
+
+    private String pistaEspecifica(int x, int y){
+        int diferencia = x - y;
+        String pistaEs;
+        if (diferencia <= 3) {
+            pistaEs = "muy caliente";
+        } if (diferencia <= 5) {
+            pistaEs = "caliente";
+        } if (diferencia <= 10) {
+            pistaEs = "casi casi";
+        } if (diferencia <= 20) {
+            pistaEs = "muy frio";
+        } else
+            pistaEs = "muy muy frio";
+        return pistaEs;
+    }
+
+    public String pista(int x) {
+        String p = "";
+        if (x > numAler){
+            p = "Mayor - " + pistaEspecifica(x, numAler);
+        } else
+            p = "Menor - " + pistaEspecifica(x, numAler);
+        intentos += 1;
+        return p;
+    }
 
 
-//-----------------------------
-import java.util.Scanner;
-    public class Main {
-        public static void main(String[] args) {
-            int numSecreto = (int) (Math.random()*100) +1;
-            int intentos = 0;
-            int intentosMax = 3;
-
-            boolean exito = false;
-            int entradaInt;
-            int difernecia;
-            String pista = "";
-
-            if (exito != true) {
-                for (int i = 0; i < intentosMax; i++) {
-                    Scanner entrada = new Scanner(System.in);
-                    entradaInt = entrada.nextInt();
-                    if (numSecreto != entradaInt) {
-                        if (entradaInt > numSecreto){
-                            pista = "Mayor - " + pistaEspecifica(entradaInt, numSecreto);
-                        } else
-                            pista = "Menor - " + pistaEspecifica(entradaInt, numSecreto);
-                        intentos += 1;
-                    } else {
-                        //gulih mabruk
-                        break;
-                    }
-                }
-                if (intentos <= intentosMax - 1) {
-
-                }
+    public void boucle() {
+        while (intentos < intentosMax) {
+            Scanner entrada = new Scanner(System.in);
+            int x = entrada.nextInt();
+            if (esExito(x)) {
+                System.out.println("congrats");
+                System.out.println("intentos: " + intentos);
+                break;
             }
-
-
-// mayor            - entrrada > num
-// menor            - entrrada < num
-//
-// muy caliente     - d < 3
-// caliente         - d < 5
-// casi casi        - d < 10
-// muy frio         - d < 20
-// muy muy frio     - d < 25
-
-
+            // EsExito not true
+            else {
+                //darle la pista
+                System.out.println(pista(x));
+                System.out.println("intentos: " + intentos);
+            }
+            if (intentos == intentosMax ) {
+                System.out.println("GAME OVER");
+            }
         }
+
     }
-//    private String pistaEspecifica(int x, int y){
-//        int diferencia = x - y;
-//        if (diferencia < 3) {
-//            return "muy caliente";
-//        } else if (diferencia < 5) {
-//            return "caliente";
-//        } else if (diferencia < 10) {
-//            return "casi casi";
-//        } else if (diferencia < 20) {
-//            return "muy frio";
-//        } else
-//            return "muy muy frio";
-//    }
-//
+
+}
+
+//---------------------------
+
+public class Main {
+    public static void main(String[] args) {
+        NumSecreto number = new NumSecreto();
+        number.mostrarNum();
+        number.boucle();
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
